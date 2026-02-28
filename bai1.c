098ef81d0d179b14eac106f1adcd25fdc8c876f7
NgocPhuong-Jelly <tranthingocphuong355@gmail.com>
2026-02-28 10:37:31 +0700

    np nopbai

--- bai1.c
+++ bai1.c
@@ -1,7 +1,7 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Trần Thị Ngọc Phương]
+ * MSSV:      [PS48607]
+ * Lớp:       [CS21302]
  *****************************************************************************/
 
 //  BÀI 1: XÂY DỰNG CHƯƠNG TRÌNH ĐẾM NGUYÊN ÂM VÀ PHỤ ÂM CỦA 1 CHUỖI 
@@ -10,5 +10,32 @@
 //  Biết rằng: Nguyên âm gồm: a,e,i,o,y,u
 
 
-// VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
 
+int main() {
+    char chuoi[1000];
+    int nguyen_am = 0, phu_am = 0;
+    int i;
+
+    printf("Nhập vào một chuỗi: ");
+    fgets(chuoi, sizeof(chuoi), stdin);
+    
+    chuoi[strcspn(chuoi, "\n")] = 0;
+
+    for (i = 0; chuoi[i] != '\0'; i++) {
+        char c = tolower(chuoi[i]);
+        if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'y') {
+            nguyen_am++;
+        } else if ((c >= 'a' && c <= 'z')) {
+            phu_am++;
+        }
+    }
+
+    printf("Số lượng nguyên âm: %d\n", nguyen_am);
+    printf("Số lượng phụ âm: %d\n", phu_am);
+
+    return 0;
+}
+ 
--- bai3.c
+++ bai3.c
@@ -1,12 +1,40 @@
 /******************************************************************************
- * Họ và tên: [ĐIỀN TÊN TẠI ĐÂY]
- * MSSV:      [ĐIỀN MSSV TẠI ĐÂY]
- * Lớp:       [ĐIỀN LỚP TẠI ĐÂY]
+ * Họ và tên: [Trần Thị Ngọc Phương]
+ * MSSV:      [PS48607]
+ * Lớp:       [CS21302]
  *****************************************************************************/
 
 //  BÀI 3: XÂY DỰNG CHƯƠNG TRÌNH SẮP XẾP CHUỖI THEO CHỮ CÁI 
 //  Input: Nhập 5 chuỗi bất kỳ 
 //  Output: Thứ tự các chuỗi đã được sắp xếp  
 
-// VIẾT CODE Ở ĐÂY
+#include <stdio.h>
+#include <string.h>
 
+int main() {
+    char s[5][20];
+    char t[20];
+    int i, j;
+    
+    printf("Nhập 5 chuỗi bất kỳ:\n");
+    for (i = 0; i < 5; i++) {
+        scanf("%s", s[i]);
+    }
+    
+    for (i = 0; i < 4; i++) {
+        for (j = 0; j < 4 - i; j++) {
+            if (strcmp(s[j], s[j + 1]) > 0) {
+                strcpy(t, s[j]);
+                strcpy(s[j], s[j + 1]);
+                strcpy(s[j + 1], t);
+            }
+        }
+    }
+    
+    printf("\nThứ tự các chuỗi đã sắp xếp:\n");
+    for (i = 0; i < 5; i++) {
+        printf("%s\n", s[i]);
+    }
+    
+    return 0;
+}
